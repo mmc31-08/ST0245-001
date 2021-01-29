087853b230a5d1349851ad1a831d990906fbcd4d
mmc31-08 <71681323+mmc31-08@users.noreply.github.com>
2021-01-29 11:18:34 -0500

    Add files via upload

---
+++ talleres/taller01/Fechas.java
@@ -0,0 +1,45 @@
+public class Fechas {
+
+    private int dia, mes, anio;
+
+    public Fechas(int dia, int mes, int anio) {
+        this.anio = anio;
+        this.dia = dia;
+        this.mes = mes;
+    }
+
+    public int anio() {
+        return this.anio;
+    }
+
+    public int dia() {
+        return this.dia;
+    }
+
+    public int mes() {
+        return this.mes;
+    }
+
+    public int comparar(Fechas f) {
+        if (this.anio < f.anio)
+            return -1;
+        else if (this.anio > f.anio)
+            return 1;
+        else if (this.mes < f.mes)
+            return -1;
+        else if (this.mes > f.mes)
+            return 1;
+        else if (this.dia < f.dia)
+            return -1;
+        else if (this.dia > f.dia)
+            return 1;
+        else{
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Fecha: " + this.dia + "/" + this.mes + "/" + this.anio;
+    }
+}
