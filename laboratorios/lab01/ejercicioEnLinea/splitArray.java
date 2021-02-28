5062343ab245efd3585c35003925ab57ad6dc9b7
mmc31-08 <71681323+mmc31-08@users.noreply.github.com>
2021-02-28 17:36:43 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/splitArray.java
@@ -0,0 +1,17 @@
+public class splitArray{
+    public boolean splitArray(int[] nums){
+        int index = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        return listaArray(nums, index, sum1, sum2);
+    }
+
+    private boolean listaArray(int[] nums, int index, int sum1, int sum2){
+        if(index>=nums.length){
+            return sum1==sum2;
+        }
+        int value = nums[index];
+        return(listaArray(nums, index +1, sum1 + value, sum2)) || (listaArray(nums, index +1, sum1, sum2 + value));
+
+    }
+}
