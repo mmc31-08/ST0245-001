c2f094aa673330dd8a180cc2d740974e2e90bc09
mmc31-08 <71681323+mmc31-08@users.noreply.github.com>
2021-03-13 17:54:04 -0500

    Add files via upload

---
+++ laboratorios/lab02/ejercicioEnLinea/Sum28.java
@@ -0,0 +1,15 @@
+public class Sum28
+{
+    public boolean sum28(int[] nums){
+        int totalSumArray = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i]==2){
+                totalSumArray+=2;
+            }
+        }
+        if (totalSumArray == 8){
+            return true;
+        }
+        return false;
+    }
+}
