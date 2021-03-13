b2f4ea2cb757f901ea320c20bdb1fddc3dd58eb2
mmc31-08 <71681323+mmc31-08@users.noreply.github.com>
2021-03-13 16:32:49 -0500

    Add files via upload

---
+++ laboratorios/lab02/codigo/insertionSort.java
@@ -0,0 +1,17 @@
+public class insertionSort
+{
+       public static int[] insertionSort (int[] array){
+        int temp;
+        for(int i = 1; i<array.length; i++){
+            for(int j = i; j>0; j--){
+                if(array[j]<array[j-1]){
+                    temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                }
+            }
+        }
+        return array;
+    }//O(n^2)
+
+}
