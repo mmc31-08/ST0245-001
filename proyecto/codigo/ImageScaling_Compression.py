0be0092a4be2faf96654f2e5f3b55c8852f3c1f7
mmc31-08 <71681323+mmc31-08@users.noreply.github.com>
2021-04-10 22:54:53 -0500

    Add files via upload

---
+++ proyecto/codigo/ImageScaling_Compression.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy as np
+
+im = np.array(Image.open("Rosa.jpg").convert("L")) 
+print(im.shape)
+
+
+factor = 2
+x = im.shape[0]//factor
+y = im.shape[1]//factor
+output = []
+
+print(im)
+
+for i in range(x):
+    row = []
+    for j in range(y):
+        row.append(im[i*factor][j*factor])
+    output.append(row)
+
+outputImage = Image.fromarray(np.array(output), "L")
+outputImage.save("CompressionRosa.jpg")
+
